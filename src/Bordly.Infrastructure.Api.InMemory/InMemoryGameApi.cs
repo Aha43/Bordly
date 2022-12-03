39090e18cefd0daf4a849f7281dcfd1373b73ffc
Arne Halvorsen <arne.halvorsen@gmail.com>
2022-12-03 13:23:02 +0100

    wip: started in memory api implementation

---
+++ src/Bordly.Infrastructure.Api.InMemory/InMemoryGameApi.cs
@@ -0,0 +1,28 @@
+﻿using Bordly.Specification.Api;
+using Bordly.Specification.Domain;
+using Bordly.Specification.Domain.Param;
+
+namespace Bordly.Infrastructure.Api.InMemory;
+
+public class InMemoryGameApi : IGameApi
+{
+    public Task<IGameModel> CreateGameAsync(ICreateGameParam param, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<IGameRowModel>> GetGameAsync(IGameParam param, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<IGameModel>> GetGamesAsync(IGamesParam param, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IGameRowModel> PlayAsync(IMoveParam param, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
